a8231e1406d003ba8de0cae8d38eb95d0eaa95e1
Matt Painter <79821595+JMPGames@users.noreply.github.com>
2021-11-14 22:42:41 -0500

    Initial Commit

---
+++ RogueOgreBattleLike/Assets/_Scripts/Entity/BattleEntity.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleEntity : MonoBehaviour {
+    //[SerializeField] Skill[] skills = new Skill[4];
+    [SerializeField] int maxHealth;
+    public int Health { get; private set; }
+}
